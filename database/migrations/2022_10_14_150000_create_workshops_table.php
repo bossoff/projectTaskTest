9d83ea127ea6bf34e088524cdc36eb13e408835f
bossoff <rajiatlive@gmail.com>
2022-10-14 16:05:20 +0100

    generating migerations

---
+++ database/migrations/2022_10_14_150000_create_workshops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->bigIncrements('id');
+            $table->string('start')->nullable();
+            $table->string('end')->nullable();
+            $table->integer('event_id')->nullable();
+            $table->integer('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
