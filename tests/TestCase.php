89965d59cd697908603a6d2c23af28ac4d72b9e2
bossoff <rajiatlive@gmail.com>
2022-10-14 15:25:10 +0100

    Method test example modified

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,15 @@
 
 namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\CreatesApplication;
 use PHPUnit\Framework\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AnotherTest extends TestCase {
+
     public function testExample() {
-        use CreatesApplication;
+
+        CreatesApplication::class;
     }
 }
